3d8815e16153899a19394bcd852b583c684b1827
Richo Healey <richo@psych0tik.net>
2013-04-19 21:45:50 +1000

    Make Use plugin do something reasonable by default

--- plugin/use.rb
+++ plugin/use.rb
@@ -1,4 +1,14 @@
 module Plugin::Use
+  def self.included(mod)
+    if mod.is_a? Class
+      mod.add_hook(:toplevel) do
+        __function("_#{name}_use") do
+          use_common
+        end
+      end
+    end
+  end
+
   def use_common
     __export("#{name}_ROOT", raw("$1"))
     __set("__#{name}_path_fragment", path_fragment)
