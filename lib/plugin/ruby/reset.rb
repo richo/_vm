338041258e518239cb4e5d2c07eb9f358a135d88
Richo Healey <richo@psych0tik.net>
2013-04-23 00:29:57 +1000

    Fix namespacing insanity

--- lib/plugin/ruby/reset.rb
+++ lib/plugin/ruby/reset.rb
@@ -1,12 +1,14 @@
-module Plugin::Ruby::Reset include Plugin::Reset
-  def self.included(mod)
-    mod.add_hook(:toplevel) do
-      main_reset_fn do
-        reset_common
-        __unset("RUBYOPT")
-        __unset("GEM_HOME")
-        __unset("GEM_ROOT")
-        __unset("GEM_PATH")
+module Plugin::Ruby
+  module Reset include Plugin::Reset
+    def self.included(mod)
+      mod.add_hook(:toplevel) do
+        main_reset_fn do
+          reset_common
+          __unset("RUBYOPT")
+          __unset("GEM_HOME")
+          __unset("GEM_ROOT")
+          __unset("GEM_PATH")
+        end
       end
     end
   end
--- lib/plugin/ruby/use.rb
+++ lib/plugin/ruby/use.rb
@@ -1,16 +1,18 @@
-module Plugin::Ruby::Use include Plugin::Use
-  def self.included(mod)
-    mod.add_hook(:toplevel) do
-      main_use_fn do
-        use_common
-        __export("RUBYOPT", raw("$2"))
-        __if(%<[ $UID -gt 0 ]>) do |c|
-          c.then do
-            gem_path = raw("$HOME/.gem/$(basename $1)")
-            __export("GEM_HOME", gem_path)
-            __export("GEM_ROOT", gem_path)
-            __export("GEM_PATH", gem_path)
-            add_to_PATH(raw("#{gem_path}/bin"))
+module Plugin::Ruby
+  module Use include Plugin::Use
+    def self.included(mod)
+      mod.add_hook(:toplevel) do
+        main_use_fn do
+          use_common
+          __export("RUBYOPT", raw("$2"))
+          __if(%<[ $UID -gt 0 ]>) do |c|
+            c.then do
+              gem_path = raw("$HOME/.gem/$(basename $1)")
+              __export("GEM_HOME", gem_path)
+              __export("GEM_ROOT", gem_path)
+              __export("GEM_PATH", gem_path)
+              add_to_PATH(raw("#{gem_path}/bin"))
+            end
           end
         end
       end
