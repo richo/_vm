3b21b71a64ab58a7ebefb8b09cd522ec36e26439
Richo Healey <richo@psych0tik.net>
2013-04-22 14:12:50 +1000

    Add a release script

---
+++ tag_release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+current_release=0
+release_series="v0.0."
+for tag in `git tag`; do
+    echo $tag | grep -Eo "${release_series}(\d+)" >/dev/null
+    [ $? -gt 0 ] && continue
+    this_tag=$(echo $tag | grep -Eo "(\d+)$")
+    [ $this_tag -gt $current_release ] && current_release=$this_tag
+done
+
+current_release=$(( $current_release + 1 ))
+
+git checkout HEAD^0
+
+mkdir dist
+for i in _*; do
+    ruby $i > dist/$i
+    [ $? -eq 0 ] || exit 1
+done
+git add dist
+git commit -m "Add dist files for ${release_series}${current_release}"
+
+git tag -s ${release_series}${current_release} -m "Release ${release_series}${current_release}"
