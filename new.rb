883384518745fce75ac9e17462b3c4a2b94e7e1c
Richo Healey <richo@psych0tik.net>
2013-11-05 21:14:12 +1100

    Switch any new version managers over to pull from ZSH

--- new.rb
+++ new.rb
@@ -8,12 +8,15 @@ proxy_type = ARGV[1] || "PosixProxy"
 puts ERB.new(DATA.read).result(binding)
 __END__
 require File.expand_path('../lib/manager', __FILE__)
+require File.expand_path('../contrib/zsh', __FILE__)
 
 class <%= class_name %> < Manager
   include <%= proxy_type %>
+  if ENV['_VM_USE_ZSH']
+    include Contrib::ZSH
+  end
 
   name "<%= name %>"
-  set_prefix "_"
 
   include Plugin::List
   # Needs to be included last of the main_case plugins
