6da147e724849a393b8cf2f489e5e3bd49312e4d
Richo Healey <richo@psych0tik.net>
2013-11-05 18:42:23 +1100

    Add include guards around core modules

--- lib/plugin/list.rb
+++ lib/plugin/list.rb
@@ -1,18 +1,20 @@
 module Plugin::List
   def self.included(mod)
-    mod.add_hook(:main_case) do |c|
-      c.when("") do
-        bare("star")
-        for_all(name, "i") do
-          __if(cmp(raw("$i"), String).eq(root)) do |ci|
-            ci.then do
-              __set('star', "*")
-            end
-            ci.else do
-              __set('star', " ")
-            end
-            end
-          echo raw(' $star $(basename $i)')
+    if mod.is_a? Class
+      mod.add_hook(:main_case) do |c|
+        c.when("") do
+          bare("star")
+          for_all(name, "i") do
+            __if(cmp(raw("$i"), String).eq(root)) do |ci|
+              ci.then do
+                __set('star', "*")
+              end
+              ci.else do
+                __set('star', " ")
+              end
+              end
+            echo raw(' $star $(basename $i)')
+          end
         end
       end
     end
--- lib/plugin/set.rb
+++ lib/plugin/set.rb
@@ -1,17 +1,19 @@
 module Plugin::Set
   def self.included(mod)
-    mod.add_hook(:main_case) do |c|
-      c.when("*") do
-        for_all(name, "i") do
-          __if(cmp(raw(basename raw("$i")), String).eq(args[1])) do |ci|
-            ci.then do
-              shift
-              __call("_#{name}_use", raw("$i"), raw("$*"))
-              __return(raw("$?"))
+    if mod.is_a? Class
+      mod.add_hook(:main_case) do |c|
+        c.when("*") do
+          for_all(name, "i") do
+            __if(cmp(raw(basename raw("$i")), String).eq(args[1])) do |ci|
+              ci.then do
+                shift
+                __call("_#{name}_use", raw("$i"), raw("$*"))
+                __return(raw("$?"))
+              end
             end
           end
+          echo raw("_#{name}: unknown #{name}: #{args[1]}")
         end
-        echo raw("_#{name}: unknown #{name}: #{args[1]}")
       end
     end
   end
