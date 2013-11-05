e29c7b9f0f2b7bf66e600e2d57ee41c143e939a2
Richo Healey <richo@psych0tik.net>
2013-11-05 19:12:13 +1100

    Setup completion by default

--- contrib/zsh.rb
+++ contrib/zsh.rb
@@ -1,7 +1,24 @@
 module Contrib
   module ZSH
     def self.included(mod)
-      mod.set_prefix ","
+      prefix = ","
+      mod.set_prefix prefix
+
+      mod.add_hook(:toplevel) do
+        completer_fn = setup_completion
+        __eval("compctl -K #{completer_fn} #{prefix}#{name}")
+      end
+    end
+
+    def setup_completion
+      fn_name = "_#{name}_completer"
+      __function(fn_name) do
+        __set("reply", bare("()"))
+        for_all(name, "i") do
+          __eval("reply+=($(basename $i))")
+        end
+      end
+      return fn_name
     end
   end
 end
