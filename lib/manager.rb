360296ae3a7321aee2f5efc8d60be48f1cb513ad
Richo Healey <richo@psych0tik.net>
2014-01-03 22:59:34 -0800

    Take a stab at implementing ,ruby::gems

--- lib/manager.rb
+++ lib/manager.rb
@@ -73,6 +73,10 @@ class Manager
     @@prefix = prefix
   end
 
+  def self.prefix
+    @@prefix
+  end
+
   def __defaults
     @defaults
   end
--- lib/plugin/ruby/gems.rb
+++ lib/plugin/ruby/gems.rb
@@ -1,5 +1,30 @@
 module Plugin::Ruby
   module Gems
+
+    def self.included(mod)
+      if mod.is_a? Class
+        mod.add_hook(:toplevel) do
+          __function("#{mod.prefix}#{name}::gems") do
+            __case(args[1]) do |c|
+              c.when("--reset") do
+                __if(__set?(root)) do |c|
+                  c.then do
+                    __use!(raw("$(basename #{root})"))
+                  end
+                  c.else do
+                    reset_gem_paths!
+                  end
+                end
+              end
+              c.when("*") do
+                set_gem_paths!(raw("#{"`pwd`"}/#{args[1]}"))
+              end
+            end
+          end
+        end
+      end
+    end
+
     def set_gem_paths!(path)
       __export("GEM_HOME", path)
       __export("GEM_ROOT", path)
