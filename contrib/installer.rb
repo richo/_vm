386fb5780c6db009f326695764f278f901ce263c
Richo Healey <richo@psych0tik.net>
2015-03-08 23:11:37 -0700

    fix args to install

--- contrib/installer.rb
+++ contrib/installer.rb
@@ -6,7 +6,7 @@ module Contrib
         script_path = @@script_path
         mod.add_hook(:main_case) do |c|
           c.when("install") do
-            __call(script_path, raw("$1"), raw("$2"))
+            __call(script_path, raw("$2"), raw("$3"))
           end
         end
       end
