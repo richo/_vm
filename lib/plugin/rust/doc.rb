437b64b7dc651501e5fc69b6e62ccf9bd6def71d
Richo Healey <richo@psych0tik.net>
2015-04-04 17:02:35 -0700

    Open a specific crates docs

--- lib/plugin/rust/doc.rb
+++ lib/plugin/rust/doc.rb
@@ -1,12 +1,22 @@
 module Plugin::Rust
   module Doc
+    def path_for(crate)
+      __escapinate(raw("#{root}/share/doc/rust/html/#{crate}/index.html"))
+    end
+
     def self.included(mod)
       if mod.is_a? Class
         mod.add_hook(:main_case) do |c|
           c.when("doc") do
-            std_root = "/share/doc/rust/html/std/index.html"
             echo raw('Loading docs from ' + root)
-            __eval("open " + __escapinate(raw(root + std_root)))
+            __case(args[2]) do |ca|
+              ca.when("") do
+                __eval("open #{path_for "std"}")
+              end
+              ca.else do
+                __eval("open #{path_for args[2]}")
+              end
+            end
           end
         end
       end
